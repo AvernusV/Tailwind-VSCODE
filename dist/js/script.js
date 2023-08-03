040adfc36fe7cfbafb2303106903f49d594b04f1
Yuuki175 <realouise175@gmail.com>
2023-08-03 10:30:27 +0800

    VSCODE 2nd day

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,5 +2,6 @@ const toggleMenu = document.querySelector(".header__menu");
 const headerNav = document.querySelector(".header__nav");
 
 toggleMenu.addEventListener("click", () => {
-    headerNav.classList.toggle('h-[23rem]');
+    headerNav.classList.toggle('h-[20.5rem]');
+    headerNav.classList.toggle('overflow-y-scroll');
 });
